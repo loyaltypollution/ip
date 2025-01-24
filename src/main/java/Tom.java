a526c33e93411afeb08a2df5556c80f1a4186ad1
loyaltypollution <65063925+loyaltypollution@users.noreply.github.com>
2025-01-24 14:39:57 +0800

    Duke.java: Rename chatbot name from Duke to Tom
    Changes made:
    * File name is renamed from Duke.java to Tom.java
    to remove all traces of Duke from the source code.
    
    * Ascii art prining Duke has been changed to Tom.

---
+++ src/main/java/Tom.java
@@ -0,0 +1,13 @@
+public class Tom {
+    public static void main(String[] args) {
+        String logo = "___________             \n"
+                    + "\\__    ___/___   _____  \n"
+                    + "  |    | /  _ \\ /     \\ \n"
+                    + "  |    |(  <_> )  Y Y  \\\n"
+                    + "  |____| \\____/|__|_|  /\n"
+                    + "                     \\/ \n";
+        System.out.println("Hello from\n" + logo);
+    }
+}
+
+
