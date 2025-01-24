a184e2a7549eeff40f04e08b879152f4a06bee48
loyaltypollution <65063925+loyaltypollution@users.noreply.github.com>
2025-01-24 15:37:55 +0800

    Tom.java: Refactor if-elseif to switch case
    Refactored the if-elseif statements into a switch-case structure.
    This change makes it easier to add additional commands in the future.

--- src/main/java/Tom.java
+++ src/main/java/Tom.java
@@ -11,27 +11,30 @@ public class Tom {
                     + "  |    |(  <_> )  Y Y  \\\n"
                     + "  |____| \\____/|__|_|  /\n"
                     + "                     \\/ \n";
-        System.out.println("Hello from\n" + logo);
+            System.out.println("Hello from\n" + logo);
         System.out.println("What can I do for you?");
 
         Scanner inputBuffer = new Scanner(System.in);
-        while (true) {
-            String userInput = inputBuffer.nextLine();
-            if (userInput.equals("bye")) {
-                break;
-            }
-            else if (userInput.equals("list")) {
-                for (int i = 0; i < taskCount; i++) {
-                    System.out.println(" " + (i + 1) + ". " + tasks[i]);
-                }
-                continue;
-            }
-            if (taskCount < 100) {
-                tasks[taskCount] = userInput;
-                taskCount++;
-                System.out.println(" added: " + userInput);
-            } else {
-                System.out.println(" Task list is full! Cannot add more tasks.");
+        boolean continueReading = true;
+        while (continueReading) {
+            String userInput = inputBuffer.next();
+            switch (userInput) {
+                case "bye":
+                    continueReading = false;
+                    break;
+                case "list":
+                    for (int i = 0; i < taskCount; i++) {
+                        System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                    }
+                    break;
+                default:
+                    if (taskCount < 100) {
+                        tasks[taskCount] = userInput;
+                        taskCount++;
+                        System.out.println(" added: " + userInput);
+                    } else {
+                        System.out.println(" Task list is full! Cannot add more tasks.");
+                    }
             }
         }
         inputBuffer.close();
@@ -39,5 +42,3 @@ public class Tom {
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
-
-
